d1e70cfce696894b71a3255e6c933adc25fb8219
Talley Lambert <talley.lambert@gmail.com>
2021-06-21 11:57:20 -0400

    add ref accuracy test

--- anyfft/tests/test_benchmarks.py
+++ anyfft/tests/test_benchmarks.py
@@ -1,7 +1,32 @@
 import numpy as np
+import numpy.testing as npt
 import pytest
+from scipy import fft, misc, signal
 
 import anyfft.fft
+
+
+@pytest.fixture(scope="session")
+def img():
+    return misc.face(gray=True)
+
+
+@pytest.fixture(
+    params=[(2 ** n,) * 2 for n in range(7, 11)], scope="session", ids=lambda x: x[0]
+)
+def random(request):
+    return np.random.random(request.param)
+
+
+@pytest.fixture(scope="session")
+def kernel():
+    return np.outer(
+        signal.windows.gaussian(70, 8), signal.windows.gaussian(70, 8)
+    ).astype("float32")
+
+
+def reference_fftn(img):
+    return fft.fftn(img)
 
 
 @pytest.mark.benchmark(warmup=False)
@@ -11,3 +36,14 @@ import anyfft.fft
 def test_bench(func, plugin, shape, benchmark):
     arr = np.zeros(shape)
     benchmark(getattr(anyfft.fft, func), arr, plugin=plugin)
+
+
+@pytest.mark.parametrize("plugin", list(anyfft.fft._PLUGINS))
+def test_accuracy(random, plugin):
+    result = anyfft.fft.fftn(random, plugin=plugin)
+    if hasattr(result, "get"):
+        result = result.get().astype(np.complex128)
+    ref = reference_fftn(random)
+    assert ref.shape == result.shape
+    assert ref.dtype == result.dtype
+    npt.assert_allclose(ref, result, rtol=4e-2)
