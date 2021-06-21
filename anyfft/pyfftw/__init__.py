d33675c313879c9ba2b6fc30070e823bc8b0ce30
Talley Lambert <talley.lambert@gmail.com>
2021-06-21 12:01:04 -0400

    update

--- anyfft/pyfftw/__init__.py
+++ anyfft/pyfftw/__init__.py
@@ -1,4 +1,7 @@
 import pyfftw.interfaces
+from pyfftw.interfaces.numpy_fft import fftn
 
 pyfftw.interfaces.cache.enable()
 pyfftw.interfaces.cache.set_keepalive_time(1)
+
+__all__ = ["fftn", "fft"]
--- anyfft/reikna/tests/test_reikna_fft.py
+++ anyfft/reikna/tests/test_reikna_fft.py
@@ -1,39 +1,38 @@
 import numpy as np
 import numpy.testing as npt
 import pytest
-from scipy import fftpack, signal
-from skimage.data import grass
+from scipy import fftpack, misc, signal
 
 import anyfft
 from anyfft.reikna._util import empty_like, to_device
 
-GRASS = grass().astype("float32")
+FACE = misc.face(gray=True).astype("float32")
 KERNEL = np.outer(
     signal.windows.gaussian(70, 8), signal.windows.gaussian(70, 8)
 ).astype("float32")
 
 
 def test_scipy_fft():
-    scipy_fgrass = fftpack.fftn(GRASS)
-    scipy_ifgrass = fftpack.ifftn(scipy_fgrass)
+    scipy_fface = fftpack.fftn(FACE)
+    scipy_ifface = fftpack.ifftn(scipy_fface)
 
-    assert not np.allclose(scipy_fgrass, GRASS, atol=100)
-    assert scipy_fgrass.dtype == np.complex64
-    assert scipy_fgrass.shape == GRASS.shape
+    assert not np.allclose(scipy_fface, FACE, atol=100)
+    assert scipy_fface.dtype == np.complex64
+    assert scipy_fface.shape == FACE.shape
     # inverse
-    npt.assert_allclose(scipy_ifgrass.real, GRASS, atol=1e-4)
+    npt.assert_allclose(scipy_ifface.real, FACE, atol=1e-4)
 
 
 def test_fft():
-    gpu_fgrass = anyfft.fftn(GRASS)
-    fgrass = gpu_fgrass.get()
+    gpu_fface = anyfft.fftn(FACE)
+    fface = gpu_fface.get()
 
-    assert not np.allclose(fgrass, GRASS, atol=100)
-    npt.assert_allclose(fgrass, fftpack.fftn(GRASS), atol=2e-1)
+    assert not np.allclose(fface, FACE, atol=100)
+    npt.assert_allclose(fface, fftpack.fftn(FACE), atol=2e-1)
 
-    gpu_ifgrss = anyfft.ifftn(gpu_fgrass)
+    gpu_ifgrss = anyfft.ifftn(gpu_fface)
     ifgrss = gpu_ifgrss.get()
-    npt.assert_allclose(ifgrss.real, GRASS, atol=1e-4)
+    npt.assert_allclose(ifgrss.real, FACE, atol=1e-4)
 
 
 def test_fft3d():
@@ -50,28 +49,28 @@ def test_fft3d():
 
 
 def test_fft_output_array():
-    input = to_device(GRASS.astype(np.complex64))
+    input = to_device(FACE.astype(np.complex64))
     out = empty_like(input)
     anyfft.fftn(input, out)
-    npt.assert_allclose(out.get(), fftpack.fftn(GRASS), atol=2e-1)
+    npt.assert_allclose(out.get(), fftpack.fftn(FACE), atol=2e-1)
 
 
 def test_fft_inplace():
-    input = to_device(GRASS.astype(np.complex64))
+    input = to_device(FACE.astype(np.complex64))
     anyfft.fftn(input, inplace=True)
-    npt.assert_allclose(input.get(), fftpack.fftn(GRASS), atol=2e-1)
+    npt.assert_allclose(input.get(), fftpack.fftn(FACE), atol=2e-1)
 
 
 def test_fft_errors():
 
     with pytest.raises(TypeError):
         # existing OCLArray must be of complex type
-        anyfft.fftn(to_device(GRASS))
+        anyfft.fftn(to_device(FACE))
 
-    anyfft.fftn(to_device(GRASS.astype(np.complex64)))
-    anyfft.fftn(GRASS)
+    anyfft.fftn(to_device(FACE.astype(np.complex64)))
+    anyfft.fftn(FACE)
 
-    input = to_device(GRASS.astype(np.complex64))
+    input = to_device(FACE.astype(np.complex64))
     out = empty_like(input)
 
     with pytest.raises(ValueError):
@@ -80,40 +79,40 @@ def test_fft_errors():
 
     with pytest.raises(ValueError):
         # cannot use inplace with numpy array
-        anyfft.fftn(GRASS, inplace=True)
+        anyfft.fftn(FACE, inplace=True)
 
 
 def test_fftshift():
-    scp_shift = fftpack.fftshift(GRASS)
-    shift = anyfft.fftshift(GRASS).get()
+    scp_shift = fftpack.fftshift(FACE)
+    shift = anyfft.fftshift(FACE).get()
     npt.assert_allclose(scp_shift, shift)
 
 
 def test_fftconvolve_same():
-    out = anyfft.fftconvolve(GRASS, KERNEL, mode="same").get()
-    scp_out = signal.fftconvolve(GRASS, KERNEL, mode="same")
+    out = anyfft.fftconvolve(FACE, KERNEL, mode="same").get()
+    scp_out = signal.fftconvolve(FACE, KERNEL, mode="same")
     assert out.shape == scp_out.shape
     assert out.dtype == scp_out.dtype
     npt.assert_allclose(out, scp_out, atol=0.2)
 
 
 def test_fftconvolve_from_oclarray():
-    out = anyfft.fftconvolve(GRASS, KERNEL, mode="same")
-    out2 = anyfft.fftconvolve(to_device(GRASS.astype("complex64")), KERNEL, mode="same")
+    out = anyfft.fftconvolve(FACE, KERNEL, mode="same")
+    out2 = anyfft.fftconvolve(to_device(FACE.astype("complex64")), KERNEL, mode="same")
     npt.assert_allclose(out.get(), out2.get(), atol=0.2)
 
 
 def test_fftconvolve_full():
-    out = anyfft.fftconvolve(GRASS, KERNEL, mode="full").get()
-    scp_out = signal.fftconvolve(GRASS, KERNEL, mode="full")
+    out = anyfft.fftconvolve(FACE, KERNEL, mode="full").get()
+    scp_out = signal.fftconvolve(FACE, KERNEL, mode="full")
     assert out.shape == scp_out.shape
     assert out.dtype == scp_out.dtype
     npt.assert_allclose(out, scp_out, atol=0.2)
 
 
 def test_fftconvolve_valid():
-    out = anyfft.fftconvolve(GRASS, KERNEL, mode="valid").get()
-    scp_out = signal.fftconvolve(GRASS, KERNEL, mode="valid")
+    out = anyfft.fftconvolve(FACE, KERNEL, mode="valid").get()
+    scp_out = signal.fftconvolve(FACE, KERNEL, mode="valid")
     assert out.shape == scp_out.shape
     assert out.dtype == scp_out.dtype
     npt.assert_allclose(out, scp_out, atol=0.2)
